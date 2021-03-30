d3b0592d4ef57f952a0c2a798253f58b68902738
Maciej <54182839+maciejpee@users.noreply.github.com>
2021-03-30 12:09:44 +0200

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,23 @@
+const staticDevCoffee = "dev-coffee-site-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/imgs",
+  "/bicycle-128.png",
+  "/bicycle-512.png",
+  "/manifest.json",
+  "/serviceWorker.js",
+  ,
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
 self.addEventListener("fetch", fetchEvent => {
   fetchEvent.respondWith(
     caches.match(fetchEvent.request).then(res => {
